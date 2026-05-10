139c13a5ffe35fe217901e855076275699a0401e
David Cosman, BSc <davemancosman@gmail.com>
2026-05-09 20:45:30 -0400

    Add formula comment for page life expectancy query
    Added a formula comment for calculating page life expectancy.

--- DBA_scripts/page_life_expectancy.sql
+++ DBA_scripts/page_life_expectancy.sql
@@ -1,3 +1,5 @@
+--formula: (Max Server Memory in GB / 4) * 300
+
 SELECT [object_name], [counter_name], [cntr_value] AS [PLE_Seconds]
 FROM sys.dm_os_performance_counters
 WHERE [object_name] LIKE '%Buffer Manager%' 
